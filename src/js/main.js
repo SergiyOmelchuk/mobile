0992b552242f1ddd6176a834a0e16e5bde3a94db
Mona Lisa <om.sergiy@gmail.com>
2019-12-24 10:55:46 +0200

    LAnding

---
+++ src/js/main.js
@@ -0,0 +1,23 @@
+$(function () {
+
+
+    $(".slider__inner, .news__slider-inner").slick({
+        nextArrow: `<button type="button" class="slick-btn slick-next"></button>`,
+        prevArrow: `<button type="button" class="slick-btn slick-prev"><a href="#"></a></button>`,
+        infinite: false,
+    });
+
+
+    $('select').styler();
+
+    $('.header__menu-btn').click(function() {
+        $('.menu ul').slideToggle();
+        $(".header__menu-btn div").toggleClass("icon-check");
+        $(".header__menu-btn div").toggleClass("icon-bars");
+    });
+
+    // $(".header__menu-btn").on("click", function(){
+    //     $(".header__menu-btn div").toggleClass("icon-check");
+    // });
+
+});
